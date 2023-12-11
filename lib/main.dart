9d5bfc6c66ecdff3aca434cd746129c923d25d04
LeDinhNhutTien <20130433@st.hcmuaf.edu.vn>
2023-12-11 21:17:00 +0700

    Khởi tạo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_news_app/news_page.dart';
+import 'package:flutter_news_app/page/home/news_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(),
-      home: NewsPage(),
+      home: const NewsPage(),
     );
   }
 }
